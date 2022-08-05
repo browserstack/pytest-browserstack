11987010959e762af5716115736e2f780e58562f
Rushabh Shroff <rushabh.s@browserstack.com>
2022-08-05 17:52:58 +0530

    remove: redundant code, update: test file name

--- tests/test_parallel.py
+++ tests/test_parallel.py
@@ -4,7 +4,6 @@ import pytest
 from selenium.webdriver.common.by import By
 
 def test_example(selenium):
-    selenium.maximize_window()
     selenium.get('https://bstackdemo.com/')
 
     # locating product on webpage and getting name of the product
@@ -21,6 +20,6 @@ def test_example(selenium):
 
     # checking whether product has been added to cart by comparing product name and marking test pass or fail
     if productText == productCartText:
-        selenium.execute_script('browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"passed","reason": ""}}')
+        selenium.execute_script('browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"passed","reason": "Test Passed Successfully"}}')
     else:
-        selenium.execute_script('browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"failed","reason": ""}}')
+        selenium.execute_script('browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"failed","reason": "Product added to the cart not same as selected"}}')
